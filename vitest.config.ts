640e66059f020af88ed5274fbf669dd78af97aba
jeffzi <16199817+jeffzi@users.noreply.github.com>
2026-04-13 22:42:21 +0200

    chore: enable coverage reporting in test suite

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     unstubEnvs: true,
+    sequence: {
+      seed: Date.now(),
+      shuffle: {
+        files: true,
+        tests: true,
+      },
+    },
     include: ["tests/**/*.test.ts"],
     coverage: {
       provider: "v8",
